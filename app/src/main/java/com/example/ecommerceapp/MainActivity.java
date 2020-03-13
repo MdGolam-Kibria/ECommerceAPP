ccecee879733e6124bf954c55561e8187232a826
Kibria92 <kibria92@gmail.com>
2020-03-13 15:47:52 +0600

    0.2

--- app/src/main/java/com/example/ecommerceapp/MainActivity.java
+++ app/src/main/java/com/example/ecommerceapp/MainActivity.java
@@ -3,14 +3,16 @@ package com.example.ecommerceapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.Toast;
-import android.widget.Toolbar;
+
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -18,29 +20,34 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
     NavigationView navbar;
-    Toolbar toolbar;
+   Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
         navbar = (NavigationView) findViewById(R.id.navBar);
-      try {
-          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-              toolbar.setNestedScrollingEnabled(true);
-          }
-      }catch (Exception e){
-          Log.d("kk",e.getMessage());
-      }
+        toolbar =(Toolbar) findViewById(R.id.tool);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+//        try {
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//                toolbar.setNestedScrollingEnabled(true);
+//            }
+//        } catch (Exception e) {
+//            Log.d("kk", e.getMessage());
+//        }
 
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);//for toggle
+        navbar.canScrollVertically(ViewGroup.SCROLLBARS_INSIDE_OVERLAY );
         navbar.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.nav_home:
                         Toast.makeText(MainActivity.this, "wow it is works", Toast.LENGTH_LONG).show();
                         return true;
@@ -57,4 +64,4 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-    }
+}
